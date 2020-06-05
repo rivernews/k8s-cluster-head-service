b14a9d1dcb5c8bc0cee03eec5b7ae661dc579466
Shaung Cheng <shaungc@umich.edu>
2020-06-05 09:13:08 +0000

    change to use postForm()

--- src/controllers/slack.controller.go
+++ src/controllers/slack.controller.go
@@ -2,17 +2,15 @@ package controllers
 
 import (
 	"errors"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
-	"github.com/gin-gonic/gin/binding"
 )
 
 type slackRequestType struct {
-	Token string `json:"token" binding:"required"`
+	Token string `json:"token"`
 }
 
 var requestFromSlackToken, requestFromSlackTokenExists = os.LookupEnv("REQUEST_FROM_SLACK_TOKEN")
@@ -22,9 +20,9 @@ var requestFromSlackToken, requestFromSlackTokenExists = os.LookupEnv("REQUEST_F
 func SlackController(c *gin.Context) {
 	log.Println("in slack controller")
 
-	body, _ := ioutil.ReadAll(c.Request.Body)
-	log.Printf("slack request body: %s", string(body))
-	return
+	// body, _ := ioutil.ReadAll(c.Request.Body)
+	// log.Printf("slack request body: %s", string(body))
+	// return
 
 	if !requestFromSlackTokenExists {
 		log.Panic(errors.New("slack token not configured"))
@@ -34,12 +32,15 @@ func SlackController(c *gin.Context) {
 		return
 	}
 
-	slackRequest := slackRequestType{}
-	if err := c.ShouldBindBodyWith(&slackRequest, binding.JSON); err != nil {
-		log.Printf("Cannot parse slack request, ignored: %s", err)
-		c.Status(http.StatusBadRequest)
-		return
-	}
+	// slackRequest := slackRequestType{}
+	// if err := c.ShouldBindBodyWith(&slackRequest, binding.MIMEPOSTForm); err != nil {
+	// 	log.Printf("Cannot parse slack request, ignored: %s", err)
+	// 	c.Status(http.StatusBadRequest)
+	// 	return
+	// }
+	channelName := c.PostForm("channel_name")
+	log.Printf("channelName: %s", channelName)
+	return
 
 	log.Printf("slack token received!")
 	if requestFromSlackToken == slackRequest.Token {
