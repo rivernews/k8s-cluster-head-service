210566e608a626db61ccba4ae0b5233146d9340e
Shaung Cheng <shaungc@umich.edu>
2020-06-05 08:17:15 +0000

    add debug log

--- src/controllers/slack.controller.go
+++ src/controllers/slack.controller.go
@@ -19,21 +19,25 @@ var requestFromSlackToken, requestFromSlackTokenExists = os.LookupEnv("REQUEST_F
 // in order to export this function you need to capitalize it
 // https://tour.golang.org/basics/3
 func SlackController(c *gin.Context) {
+	log.Println("in slack controller")
+
 	if !requestFromSlackTokenExists {
 		log.Panic(errors.New("slack token not configured"))
 		c.JSON(http.StatusBadRequest, gin.H{
 			"reason": "slack auth token not set",
 		})
+		return
 	}
 
 	slackRequest := slackRequestType{}
 	if err := c.ShouldBindBodyWith(&slackRequest, binding.JSON); err == nil {
-		log.Printf("slack token: %s", slackRequest.Token)
+		log.Printf("slack token received!")
 
 		if requestFromSlackToken == slackRequest.Token {
 			c.JSON(http.StatusOK, gin.H{
 				"text": "K8s header service response:\n```received!```\n",
 			})
+			return
 		}
 	}
 
