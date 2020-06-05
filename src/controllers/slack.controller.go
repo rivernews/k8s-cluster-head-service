9c966f82e333e00a984529ba14bd9ddf554596bb
Shaung Cheng <shaungc@umich.edu>
2020-06-05 10:10:49 +0000

    fix err nil crash

--- src/controllers/slack.controller.go
+++ src/controllers/slack.controller.go
@@ -61,12 +61,14 @@ func SlackController(c *gin.Context) {
 		res, err := http.Post(circleCiRequestURL.String(), "application/json", buf)
 
 		var slackMessage strings.Builder
-		slackMessage.WriteString("K8s header service triggered circle ci job, response:\n```")
+		slackMessage.WriteString("K8s header service triggered circle ci job, response:\n```\n")
 		bytesContent, _ := ioutil.ReadAll(res.Body)
 		slackMessage.WriteString(string(bytesContent))
-		slackMessage.WriteString("```\nAny error:\n```")
-		slackMessage.WriteString(err.Error())
-		slackMessage.WriteString("```\n")
+		slackMessage.WriteString("\n```\nAny error:\n```\n")
+		if err != nil {
+			slackMessage.WriteString(err.Error())
+		}
+		slackMessage.WriteString("\n```\n")
 		c.JSON(http.StatusOK, gin.H{
 			"text": slackMessage.String(),
 		})
