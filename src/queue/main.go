6ef1b0e4096411def56390dff4de49a99e7818c6
Shaung <shaungc@umich.edu>
2020-07-18 23:34:24 +0000

    add test queue job

--- src/queue/main.go
+++ src/queue/main.go
@@ -53,7 +53,7 @@ func TestJobQueue() {
 	// enqueue
 
 	// Make an enqueuer with a particular namespace
-	// var enqueuer = work.NewEnqueuer("my_app_namespace", utilities.RedisPool)
+	var enqueuer = work.NewEnqueuer("my_app_namespace", utilities.RedisPool)
 
 	// worker pool
 
@@ -84,9 +84,9 @@ func TestJobQueue() {
 
 	// enqueue jobs
 	// Enqueue a job named "send_email" with the specified parameters.
-	// for i := 1; i < 5; i++ {
-	// 	enqueuer.Enqueue("send_email", work.Q{"address": "test@example.com", "subject": "hello world", "customer_id": 4})
-	// }
+	for i := 1; i < 2; i++ {
+		enqueuer.Enqueue("send_email", work.Q{"address": "test@example.com", "subject": "hello world", "customer_id": 4})
+	}
 	// enqueuer.Enqueue("guided_k8s_s3_elastic_session", work.Q{})
 
 	// Wait for a signal to quit:
